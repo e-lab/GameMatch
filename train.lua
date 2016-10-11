8324a07f5b617d0a38c533fe2fc64bdcff408932
Eugenio <culurciello@gmail.com>
2016-10-11 16:07:21 -0400

    change frees

--- train.lua
+++ train.lua
@@ -24,9 +24,9 @@ opt = lapp [[
   -w,--weightDecay        (default 0)         L2 penalty on the weights
   -m,--momentum           (default 0.9)       momentum parameter
   --steps                 (default 1e5)       number of training steps to perform
-  --epsiFreq              (default 1e5)       epsilon update
+  --epsiFreq              (default 1e4)       epsilon update
   --progFreq              (default 1e2)       frequency of progress output
-  --saveFreq              (default 5e4)       the model is saved every save_freq steps
+  --saveFreq              (default 1e4)       the model is saved every save_freq steps
 
   Model parameters:
   --lstmLayers            (default 1)     number of layers of RNN / LSTM
