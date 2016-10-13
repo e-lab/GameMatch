10b1cb0a4f9f53bad2d5054e4bbc7219e6128027
Eugenio <culurciello@gmail.com>
2016-10-12 20:39:34 -0400

    still fixing this

--- train.lua
+++ train.lua
@@ -17,7 +17,7 @@ opt = lapp [[
   --pool_frms_type      (default 'max')             pool inputs frames mode
   --pool_frms_size      (default '2')                 pool inputs frames size
   --actrep              (default 1)                 how many times to repeat action
-  --random_starts       (default 0)                 play action 0 between 1 and random_starts number of times at the start of each training episode
+  --randomStarts        (default 0)                 play action 0 between 1 and random_starts number of times at the start of each training episode
   --gamma               (default 0.975)             discount factor in learning
   --epsilon             (default 1)                 initial value of ϵ-greedy action selection
   
@@ -29,8 +29,8 @@ opt = lapp [[
   -w,--weightDecay        (default 0)         L2 penalty on the weights
   -m,--momentum           (default 0.9)       momentum parameter
   --batchSize             (default 128)       batch size for training
-  --ERBuffer              (default 1024)      Experience Replay buffer memory
-  --update_freq           (default 4)         learn every update_freq steps of game
+  --ERBufSize             (default 256)       Experience Replay buffer memory
+  --QLearnFreq            (default 4)         learn every update_freq steps of game
   --steps                 (default 1e5)       number of training steps to perform
   --epsiFreq              (default 1e5)       epsilon update
   --progFreq              (default 1e2)       frequency of progress output
@@ -50,6 +50,7 @@ opt = lapp [[
 
 -- format options:
 opt.pool_frms = 'type=' .. opt.pool_frms_type .. ',size=' .. opt.pool_frms_size
+opt.learnStart = opt.batchSize -- we shoudl not start training until we have enough to fill a batch
 
 if opt.verbose >= 1 then
     print('Using options:')
@@ -102,8 +103,10 @@ end
 
 -- online training: algorithm from: http://outlace.com/Reinforcement-Learning-Part-3/
 local win = nil
-local input, output, target, value, action_index
+local input, output, target, value, action_index, state, action, new_state
 local buffer = {} -- Experience Replay buffer
+input = torch.Tensor(opt.batchSize, 3, 84, 84)
+if opt.useGPU then input = input:cuda() end
 
 print("Started training...")
 while step < opt.steps do
@@ -121,58 +124,84 @@ while step < opt.steps do
     return f, dE_dw -- return f and df/dX
   end
 
-  local function modelEval()
-      input = image.scale(screen[1], 84, 84, 'bilinear') -- scale image to smaller size
-      if opt.useGPU then input = input:cuda() end
-      output = model:forward(input)
+  local function scaleOneIm(im) -- scale one image
+    return image.scale(im[1], 84, 84, 'bilinear')
   end
 
-  local function QLearn()
-    target = output:clone() -- copy previous output as target
-
-    -- observe Q(S',a)
-    if not terminal then
-      modelEval()
-      value, action_index = output:max(1)
-      update = reward + gamma*value
-      target[action_index[1]] = update -- target is previous output updated with reward
-
-      -- then train neural net:
-      _,fs = optim.adam(eval_E, w, optimState)
-      err = err + fs[1]
+  local function scaleBIm(im) -- scale batch of images
+    for i=1,im:size(1) do
+      input[i] = image.scale(im[i], 84, 84, 'bilinear') -- scale image to smaller size
     end
+    return input
+  end
+
+  local function modelEval(im) -- forward model
+      local input = scaleBIm(im)
+      output = model:forward(input)
+      return output
+  end
+
+  local function getNextBatch()
+    local ri = torch.randperm(opt.ERBufSize)
+    for i=1,opt.batchSize do
+      input[i] = buffer[ri[i]].state
+    end
+    target = output:clone() -- copy previous output as 
+    output = model:forward(input) --modelEval(input)
+    for i=1,opt.batchSize do
+      -- observe Q(S',a)
+      if not buffer[ri[i]].terminal then
+        value, action_index = output[i]:max(1)
+        update = buffer[ri[i]].reward + gamma*value
+      else
+        update = buffer[ri[i]].reward
+      end
+      target[i][buffer[ri[i]].action] = update -- target is previous output updated with reward
+    end
+    if opt.useGPU then target = target:cuda() end
+    return target
   end
       
-  -- We are in state S
-  -- use model to get next action: Q function on S to get Q values for all possible actions
-  modelEval()
+  -- We are in state S, now use model to get next action:
+  state = screen:clone()
+  local outNet = modelEval(screen)
 
   -- at random chose random action or action from neural net: best action from Q(S,a)
   if math.random() < epsilon then
     action_index = math.random(#game_actions) -- random action
   else
-    value, action_index = output:max(1) -- select max output
+    value, action_index = outNet:max(1) -- select max output
     action_index = action_index[1] -- select action from neural net
   end
 
   -- make the move:
   if not terminal then
+      print(action_index)
       screen, reward, terminal = game_env:step(game_actions[action_index], true)
   else
-      if opt.random_starts > 0 then
+      if opt.randomStarts > 0 then
           screen, reward, terminal = game_env:nextRandomGame()
       else
           screen, reward, terminal = game_env:newGame()
       end
   end
+  new_state = screen:clone()
   if reward ~= 0 then
     nrewards = nrewards + 1
     total_reward = total_reward + reward
   end
 
+  -- store episode in rolling buffer memory:
+  buffer[step%opt.ERBufSize+1] = {state=scaleOneIm(state), action=action_index, 
+            reward=reward, nstate=scaleOneIm(new_state), terminal=terminal}
+
   -- Q-learning updates every few steps:
-  if step % opt.update_freq == 0 then
-    QLearn()
+  if step % opt.QLearnFreq == 0 and step > opt.learnStart  then
+    print('step', step)
+    target = getNextBatch()
+    -- then train neural net:
+    _,fs = optim.adam(eval_E, w, optimState)
+    err = err + fs[1]
   end
 
   if step % opt.progFreq == 0 then
