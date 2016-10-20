f5cac78143d8a36add6fb8855f40d7a6a26525f0
Eugenio <culurciello@gmail.com>
2016-10-20 11:11:22 -0400

    clipping rewards and max

--- torch-flappy/qlearn.py
+++ torch-flappy/qlearn.py
@@ -146,6 +146,9 @@ def trainNetwork(model,args):
                 targets[i] = model._forward( torch.fromNumpyArray(state_t)._float() ).asNumpyArray()
                 Q_sa = model._forward( torch.fromNumpyArray(state_t1)._float() ).asNumpyArray()
 
+                reward_t = np.clip(reward_t,-1,1) # clip reward to keep neural network from exploding
+                Q_sa = np.clip(Q_sa,-1,1)
+
                 if terminal:
                     targets[i, action_t] = reward_t
                 else:
