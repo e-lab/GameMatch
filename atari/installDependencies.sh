d8eb46d23bb152a251da96968791b8599e3b5e45
spk <spkim921@gmail.com>
2016-11-13 10:36:39 -0500

    fix little prefix for luarkocks make

--- atari/installDependencies.sh
+++ atari/installDependencies.sh
@@ -1,11 +1,10 @@
 mkdir tmp
-PREFIX=$PWD/torch
 echo "Installing Xitari ... "
 cd /tmp
 rm -rf xitari
 git clone https://github.com/deepmind/xitari.git
 cd xitari
-$PREFIX/bin/luarocks make
+luarocks make
 RET=$?; if [ $RET -ne 0 ]; then echo "Error. Exiting."; exit $RET; fi
 echo "Xitari installation completed"
 
@@ -13,7 +12,7 @@ echo "Installing Alewrap ... "
 cd /tmp
 rm -rf alewrap
 git clone https://github.com/deepmind/alewrap.git
-cd alewrap
-$PREFIX/bin/luarocks make
+cd Alewrap
+luarocks make
 RET=$?; if [ $RET -ne 0 ]; then echo "Error. Exiting."; exit $RET; fi
 echo "Alewrap installation completed"
