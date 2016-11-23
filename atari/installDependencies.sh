e765e03ea7b03835eed33663b62811765d60e9d8
spk <spkim921@gmail.com>
2016-11-23 18:54:03 -0500

    add DIR for luarocks pointer

--- atari/installDependencies.sh
+++ atari/installDependencies.sh
@@ -1,18 +1,20 @@
+#This is for elab since elab is root account which has torch under /home/elab
+DIR=/home/elab/torch/install/bin/
 mkdir tmp
 echo "Installing Xitari ... "
-cd /tmp
+cd tmp
 rm -rf xitari
 git clone https://github.com/deepmind/xitari.git
 cd xitari
-luarocks make
+$DIR/luarocks make
 RET=$?; if [ $RET -ne 0 ]; then echo "Error. Exiting."; exit $RET; fi
 echo "Xitari installation completed"
 
 echo "Installing Alewrap ... "
-cd /tmp
+cd tmp
 rm -rf alewrap
 git clone https://github.com/deepmind/alewrap.git
 cd alewrap
-luarocks make
+$DIR/luarocks make
 RET=$?; if [ $RET -ne 0 ]; then echo "Error. Exiting."; exit $RET; fi
 echo "Alewrap installation completed"
