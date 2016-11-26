1b3c9df3a60481faac28d86ae71a0c4d0641e9dc
Eugenio <culurciello@gmail.com>
2016-11-26 14:55:58 -0500

    restored original init

--- atari/initenv.lua
+++ atari/initenv.lua
@@ -21,6 +21,11 @@ function gameEnvSetup(_opt)
     -- convert options strings to tables
     _opt.pool_frms = str_to_table(_opt.pool_frms)
     _opt.env_params = str_to_table(_opt.env_params)
+    _opt.agent_params = str_to_table(_opt.agent_params)
+    if _opt.agent_params.transition_params then
+        _opt.agent_params.transition_params =
+            str_to_table(_opt.agent_params.transition_params)
+    end
 
     -- load training framework and environment
     local framework = assert(require(opt.framework))
@@ -28,11 +33,25 @@ function gameEnvSetup(_opt)
     local gameEnv = framework.GameEnvironment(opt)
     local gameActions = gameEnv:getActions()
 
-    return gameEnv, gameActions
+    -- agent options
+    _opt.agent_params.actions   = gameActions
+    _opt.agent_params.gpu       = _opt.gpu
+    _opt.agent_params.best      = _opt.best
+    if _opt.network ~= '' then
+        _opt.agent_params.network = _opt.network
+    end
+    _opt.agent_params.verbose = opt.verbose
+    if not _opt.agent_params.state_dim then
+        _opt.agent_params.state_dim = gameEnv:nObsFeature()
+    end
+
+    return gameEnv, gameActions, agent, opt
 end
 
 
+
 --- other functions
+
 function str_to_table(str)
     if type(str) == 'table' then
         return str
