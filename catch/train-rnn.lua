12dd46bf8bad4ccbae1074d59e95d7441f4c5291
Eugenio <culurciello@gmail.com>
2016-12-16 11:33:55 -0500

    added batch inning rnn state to mem
    this is the way to do it properly, but somehow
    using: state = unpack(RNNh0Batch) during batch learning is faster and
    better….

--- catch/train-rnn.lua
+++ catch/train-rnn.lua
@@ -56,6 +56,7 @@ torch.manualSeed(opt.seed)
 os.execute('mkdir '..opt.saveDir)
 print('Playing Catch game with RNN\n')
 
+local noActionIdx = 2 -- this is the idle action: do nothing in this game
 local epsilon = opt.epsilon
 local nbActions = opt.nbActions
 local gridSize = opt.gridSize
@@ -71,21 +72,24 @@ local colors = sys.COLORS
 local memory = {}
 local function ReplayMemory(capacity)
     local channels = 1
-    memory.s = torch.zeros(capacity, opt.nSeq, nbStates)
-    memory.a = torch.zeros(capacity, opt.nSeq)
+    memory.s = torch.zeros(capacity, opt.nSeq, nbStates) -- state
+    memory.a = torch.zeros(capacity, opt.nSeq) -- action
+    memory.rs = torch.zeros(capacity, opt.nHidden) -- RNN state
 
     memory.capacity = capacity
     memory.size = 0
     memory.pos = 1
 
     -- batch buffers:
-    memory.bs = torch.zeros(opt.batchSize, opt.nSeq, nbStates)
-    memory.ba = torch.zeros(opt.batchSize, opt.nSeq)
+    memory.bs = torch.zeros(opt.batchSize, opt.nSeq, nbStates) -- state 
+    memory.ba = torch.zeros(opt.batchSize, opt.nSeq) -- action
+    memory.brs = torch.zeros(opt.batchSize, opt.nHidden) -- RNN state
     
-    function memory.addTransition(state, action)
+    function memory.addTransition(state, action, RNNstate)
         if memory.pos == 0 then memory.pos = 1 end -- tensors do not have 0 index items!
         memory.s[memory.pos] = state:clone()
-        memory.a[memory.pos] = action--:clone()
+        memory.a[memory.pos] = action:clone()
+        memory.rs[memory.pos] = RNNstate:clone()
        
         memory.pos = (memory.pos + 1) % memory.capacity
         memory.size = math.min(memory.size + 1, memory.capacity)
@@ -96,8 +100,9 @@ local function ReplayMemory(capacity)
         for i=1, math.min(sampleSize,memory.size) do
             memory.bs[i] = memory.s[ri[i]]
             memory.ba[i] = memory.a[ri[i]]
+            memory.brs[i] = memory.rs[ri[i]]
         end
-        return memory.bs, memory.ba
+        return memory.bs, memory.ba, memory.brs
     end
 
 end
@@ -159,7 +164,8 @@ local function learnBatch(seqs, targets, state)
     local function feval(x_new)
         local loss = 0
         local grOut = {}
-        local inputs = { seqs, table.unpack(state) } -- attach RNN states to input
+        -- state = unpack(RNNh0Batch)
+        local inputs = { seqs, state } -- attach RNN states to input
         local out = model:forward(inputs)
         -- process each sequence step at a time:
         for i = 1, opt.nSeq do
@@ -169,12 +175,12 @@ local function learnBatch(seqs, targets, state)
         end
         table.insert(grOut, state) -- attach RNN states to grad output
         model:backward(inputs, grOut)
+
         return loss, gradParameters
     end
 
-    local _, fs = optim.rmsprop(feval, params, sgdParams)
-
-    return fs[1] -- loss
+    local _, loss = optim.rmsprop(feval, params, sgdParams)
+    return loss[1]
 end
 
 -- reset RNN prototype state
@@ -193,21 +199,22 @@ local function fwdProto(state)
     local action = index[1]
     RNNhProto = q[1] -- next prototype state feeds back to prototype
     
-    return action
+    return action, RNNhProto
 end
 
 -- Learns from a single transition (making use of replay memory):
 local function learnFromMemory()
     -- Get a random minibatch from the replay memory and learns from it:
     if memory.size > opt.batchSize then
-        local s, a = memory.getSample(opt.batchSize)
-        learnBatch(s, a, RNNh0Batch) -- states and actions are inputs and targets to learn
+        local s, a, rs = memory.getSample(opt.batchSize)
+        learnBatch(s, a, rs) -- states and actions are inputs and targets to learn
     end
 end
 
 local steps = 1 -- counts steps to game win
 local sSeq = torch.zeros(opt.nSeq, nbStates) -- store sequence of states in successful run
 local aSeq = torch.ones(opt.nSeq) -- store sequence of actions in successful run
+local initRNNstate -- to store RNN state in memory
 local function performLearningStep(epoch)
     -- Makes an action according to eps-greedy policy, observes the result
     -- (next state, reward) and learns from the transition
@@ -230,17 +237,16 @@ local function performLearningStep(epoch)
         end
     end
 
-    local function resetSeqs() steps = 1 sSeq:zero() aSeq:fill(1) end
+    local function resetSeqs() steps = 1 sSeq:zero() aSeq:fill(noActionIdx) end -- fill with noActionIdx
 
+    local a, reward, gameOver, RNNstate
     local state = game.observe()
-
     -- With probability eps make a random action:
     local eps = explorationRate(epoch)
-    
     -- Choose the best action according to the network:
-    local a, reward, gameOver
-    a = fwdProto(state)
-    
+    a, RNNstate = fwdProto(state)
+    if steps == 1 then initRNNstate = RNNstate:clone() end -- save initial RNN state to be used in sequence learning!
+
     -- online learning: chose random action:
     if torch.uniform() <= eps then
         a = torch.random(1, nbActions)
@@ -249,21 +255,19 @@ local function performLearningStep(epoch)
 
     -- save sequences:
     sSeq[steps] = state:clone()
-    aSeq[steps] = a 
-    
+    aSeq[steps] = a
     -- if it is a successful sequence, record it and the learn
     if reward > 0 then 
         -- Remember the transition that was just experienced:
-        memory.addTransition(sSeq, aSeq)
-        -- reset step counter and sequence buffers:
-        resetSeqs()
+        memory.addTransition(sSeq, aSeq, initRNNstate)
         -- learning step:
         learnFromMemory()
-    else 
+    else
         steps = steps+1
-        if steps > opt.nSeq then
-            resetSeqs()
-        end
+    end
+    if reward ~= 0 or steps > opt.nSeq then
+        -- reset step counter and sequence buffers:
+        resetSeqs()
     end
 
     return eps, gameOver, reward
@@ -351,7 +355,7 @@ local function main()
     end 
     print("Saving the network weigths to:", opt.saveDir)
             torch.save(opt.saveDir..'/model-catch-dqn.net', model:clone():float():clearState())
-    -- game:close()
+    -- game.close()
 
     print("======================================")
     print("Training finished. It's time to watch!")
@@ -378,7 +382,7 @@ local function main()
         sys.sleep(1)
         print("Total score: ", score)
     end
-    -- game:close()
+    -- game.close()
 end
 
 -- run main program:
